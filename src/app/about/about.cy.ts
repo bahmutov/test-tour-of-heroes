e89a9d250b6721829fdd413e5161eb13c3322723
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-08-20 16:21:28 -0400

    adding a component test

---
+++ src/app/about/about.cy.ts
@@ -0,0 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
+import { TwainComponent } from './../twain/twain.component';
+import { TwainService } from '../twain/twain.service';
+import {AboutComponent} from './about.component'
+
+describe('About', () => {
+  it('mounts', () => {
+    cy.intercept('GET', '**quotes**', {
+      statusCode: 200,
+      body: {
+        id: 1,
+        quote: 'This is a test quote'
+      }
+    })
+    cy.mount(AboutComponent, {
+      imports: [HttpClientModule],
+      declarations: [TwainComponent],
+      providers: [TwainService]
+    })
+  })
+})
